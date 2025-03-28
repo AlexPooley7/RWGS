b3cfe92bbce8117de7dd5d024b7f71099e63e1d4
Alex Pooley <ap3076@bath.ac.uk>
2025-03-28 13:35:20 +0000

    Main file created with introduction

---
+++ main.m
@@ -0,0 +1,21 @@
+% CE30243 - Individual Design Project
+% Description - Models the transient behaviour of the reverse water-gas
+% shift reaction (RWGS)
+% Last edited: 28/03/2025
+% Last commit: 28/03/2025
+% Author: Alexander Pooley
+% -------------------------------------------------------------
+% Functions:
+% 
+% -------------------------------------------------------------
+% Structures:
+% 
+% -------------------------------------------------------------
+% Engineering variables:
+% 
+% -------------------------------------------------------------
+
+clc
+clear
+
+% Define constants:
